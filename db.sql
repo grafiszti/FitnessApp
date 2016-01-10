657af683c56860f064da0acadc0c25a416761792
Matt <grafiszti@gmail.com>
2016-01-10 18:23:53 +0100

    init commit, simple app config, mysql database creation sql

---
+++ db.sql
@@ -0,0 +1,65 @@
+CREATE TABLE Contract_type(
+	id int,
+	name varchar(150),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Employee (
+	id int,
+	name varchar(100),
+	surname varchar(100),
+	date_of_employment date,
+	contract_type_id int,
+	salary_hour int,
+	PRIMARY KEY (id),
+	CONSTRAINT `contract_type_id_f` FOREIGN KEY (contract_type_id) 
+		REFERENCES Contract_type (id)
+);
+
+CREATE TABLE Customer(
+	id int,
+	name varchar(100),
+	surname varchar(100),
+	adress varchar(300),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Equipment_type(
+	id int,
+	name varchar(100),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Equipment(
+	id int,
+	serial_number varchar(100),
+	name varchar(100),
+	type_id int,
+	purchase_date date,
+	last_service_date date,
+	is_working boolean,
+	PRIMARY KEY (id),
+	CONSTRAINT `type_id_f` FOREIGN KEY (type_id) 
+		REFERENCES Equipment_type (id)
+);
+
+CREATE TABLE Membership_type(
+	id int,
+	name varchar(100),
+	length_days int,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Membership(
+	id int,
+	customer_id int,
+	membership_type_id int,
+	start_date date,
+	price decimal,
+	PRIMARY KEY (id),
+	CONSTRAINT `Membership_type_id_f` FOREIGN KEY (membership_type_id) 
+		REFERENCES Membership_type (id)
+);
+
+
+
---
+++ src/test/java/pl/grafiszti/fitness/ApplicationTests.java
@@ -0,0 +1,20 @@
+//package pl.grafiszti.fitness;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import pl.grafiszti.fitness.config.RootConfig;
+//import pl.grafiszti.fitness.config.WebConfig;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = {RootConfig.class, WebConfig.class})
+//@WebAppConfiguration
+//public class ApplicationTests {
+//
+//  @Test
+//  public void contextLoads() {}
+//
+//}
